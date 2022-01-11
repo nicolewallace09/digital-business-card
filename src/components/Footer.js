ba2c36dfbcf66bf1923b6b0b26a5f9550f356b0f
nicolewallace09 <nicole.elisaw@gmail.com>
2022-01-10 23:04:19 -0800

    starting on footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,19 @@
+import TwitterIcon from '../images/Twitter-Icon.png';
+import GithubIcon from '../images/GitHub-Icon.png';
+
 const Footer = () => {
     return (
         <div className='footer'>
-    
+            <img 
+                src={GithubIcon} 
+                alt='github-icon'
+                className='footer--github'
+            />
+            <img 
+                src={TwitterIcon} 
+                alt='twitter-icon'
+                className='footer--twitter'
+            />
         </div>
     )
 }
